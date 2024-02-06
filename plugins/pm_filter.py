c67eb8fdf0f85e2c7126bfa801c4d6987b73332a
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2024-02-06 14:46:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2151,8 +2151,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('Back', callback_data='1988')
 	]]
         reply_markup = InlineKeyboardMarkup(buttons)
-	await query.message.delete()
-        await query.message.reply_photo(
+        await query.message.edit(
 		photo="https://graph.org/file/d3553fb949c9cfab3fb50.jpg",
                 caption=(script.LUV_KUSH),
                 reply_markup=reply_markup,
