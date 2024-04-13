2e6aa9491928e48222ea2770811cdc11f4b61136
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2024-04-13 11:55:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -128,7 +128,9 @@ async def start(client, message):
         for msg in msgs:
             title = msg.get("title")
             size=get_size(int(msg.get("size", 0)))
-            f_caption=msg.get("caption", "")
+            files = files_[0]
+            f_caption=files.caption
+            #f_caption=msg.get("caption", "")
             #if BATCH_FILE_CAPTION:
             #    try:
             #        f_caption=BATCH_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
